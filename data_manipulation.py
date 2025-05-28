1a2edf6df98ad03ed83ed0b869362d0e0ec84b19
mehdi <mehdi.habibian@snapp.cab>
2025-05-28 06:29:18 +0330

    feat: create a data manipulation tool

---
+++ data_manipulation.py
@@ -0,0 +1,26 @@
+"""
+In the below code we practice data manipulation using pandas and numpy library."""
+
+
+# Data URL :https://www.kaggle.com/datasets/arnavgupta1205/usa-housing-dataset?resource=download
+
+# Library:
+from pandas import read_csv
+
+import matplotlib.pyplot as plt
+
+from utility import BASE_DIR
+
+
+""" Functions: """
+
+
+def read_csv_file(path, file_name):
+    df = read_csv(path + file_name)
+    return df
+
+
+df = read_csv_file(BASE_DIR + "/data/", "usa_housing_kaggle.csv")
+df.Price.plot(kind="hist")
+plt.show()
+
